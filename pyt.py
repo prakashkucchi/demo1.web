cd09b9b8d61c9c601bead830cf3520960931310d
Prakash Rajak <155745411+prakashkucchi@users.noreply.github.com>
2025-04-03 00:22:17 +0530

    Add files via upload

---
+++ pyt.py
@@ -0,0 +1,20 @@
+import streamlit as st
+import pandas as pd 
+
+st.title("welcome to melebaby moni:", )
+name = st.text_input("Enter your name:")
+age = st.number_input("Enter your age:", min_value=0)
+fname = st.text_input("Enter your father name:")
+Adress = st.text_area("Enter your adress:")
+classdata = st.selectbox("enter your class:", (1,2,3,4,5,6,7,8,9))
+
+button = st.button("done")
+if button:
+    st.markdown(f"""
+name:{name}
+father:{fname}
+address:{Adress}
+classs:{classdata}""")
+
+
+
